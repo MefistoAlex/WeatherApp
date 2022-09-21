77917a10d64045d6784514a4a48fb70b14cda5cb
Mefisto Alex <Alexandr.mefisto@gmail.com>
2022-09-21 19:34:48 +0300

    rename ViewController to WeatherViewController

--- WeatherApp/WeatherViewController.swift
+++ WeatherApp/WeatherViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  WeatherViewController.swift
 //  WeatherApp
 //
 //  Created by Alexandr Mefisto on 21.09.2022.
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class WeatherViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
