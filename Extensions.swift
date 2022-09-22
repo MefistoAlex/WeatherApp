39a8ffa5fe04425f7ebe8017bd92cfa077731608
Mefisto Alex <Alexandr.mefisto@gmail.com>
2022-09-22 11:05:57 +0300

    Create Extensions.swift

---
+++ Extensions.swift
@@ -0,0 +1,14 @@
+//
+//  Extensions.swift
+//  WeatherApp
+//
+//  Created by Alexandr Mefisto on 22.09.2022.
+//
+
+import Foundation
+enum NavigationImages: String {
+    case place = "ic_place"
+    case location = "ic_my_location"
+    case search = "ic_search"
+    case back = "ic_back"
+}
