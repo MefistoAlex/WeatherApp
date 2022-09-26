d22d300d41429424ae37b087cc6f1d9722d30425
Alex Mefisto <alexandr.mefisto@gmail.com>
2022-09-26 15:34:35 +0300

    added winds direcnion enum

--- WeatherApp/Supporting Files/Extensions.swift
+++ WeatherApp/Supporting Files/Extensions.swift
@@ -12,3 +12,14 @@ enum NavigationImages: String {
     case search = "ic_search"
     case back = "ic_back"
 }
+
+enum WindDirection: String {
+    case west = "icon_wind_w"
+    case northWest = "icon_wind_wn"
+    case north = "icon_wind_n"
+    case northEast = "icon_wind_ne"
+    case east = "icon_wind_e"
+    case southEast = "icon_wind_se"
+    case south = "icon_wind_s"
+    case southWest = "icon_wind_ws"
+}
