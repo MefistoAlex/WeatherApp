ed1df4e4eb6e3bf69d583beb006b08c0765392e0
Alex Mefisto <alexandr.mefisto@gmail.com>
2022-09-26 16:08:33 +0300

    added WeatherIcons enum

--- WeatherApp/Supporting Files/Extensions.swift
+++ WeatherApp/Supporting Files/Extensions.swift
@@ -23,3 +23,16 @@ enum WindDirection: String {
     case south = "icon_wind_s"
     case southWest = "icon_wind_ws"
 }
+
+enum WeatherIcons: String {
+    case brigthDay = "ic_white_day_bright"
+    case cloudyDay = "ic_white_day_cloudy"
+    case rainDay = "ic_white_day_rain"
+    case showerDay = "ic_white_day_shower"
+    case thunderDay = "ic_white_day_thunder"
+    case brigthNight = "ic_white_night_bright"
+    case cloudyNight = "ic_white_night_cloudy"
+    case rainNight = "ic_white_night_rain"
+    case showerNight = "ic_white_night_shower"
+    case thunderNight = "ic_white_night_thunder"
+}
