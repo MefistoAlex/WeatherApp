363027700a98075feb7919294add05f0eb21b382
Alex Mefisto <alexandr.mefisto@gmail.com>
2022-09-27 17:17:43 +0300

    added fetching weather from map pin

--- WeatherApp/Controllers/MapViewController.swift
+++ WeatherApp/Controllers/MapViewController.swift
@@ -11,43 +11,53 @@ import UIKit
 
 final class MapViewController: UIViewController, UIGestureRecognizerDelegate {
     // MARK: - Variable Declarations
-
-    @IBOutlet var searchBar: UISearchBar!
+    private let locationManager = LocationManager.shared
+    private let weatherViewModel = WeatherViewModel.shared
+    private let searchController = UISearchController()
+    private var currentLocation: CLLocation?
+    
     @IBOutlet private var mapView: MKMapView!
-    var locationManager: CLLocationManager!
-    var currentLocationStr = "Current location"
-
+    
+    //MARK: Lyfecycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
         view.backgroundColor = UIColor(named: "DarkBlue")
 
+        currentLocation = locationManager.location
         mapView.showsScale = true
         mapView.showsCompass = true
         mapView.showsUserLocation = true
-        let search = UISearchController(searchResultsController: self)
 
-        navigationItem.searchController = search
+        searchController.searchResultsUpdater = self
+        
+        navigationItem.searchController = searchController
+
+        // set right bar button
+        let seaarhcButton = UIButton(type: .system)
+        seaarhcButton.setImage(UIImage(named: NavigationImages.search.rawValue), for: .normal)
+        seaarhcButton.sizeToFit()
+        seaarhcButton.addTarget(self, action: #selector(searchButtonDidTap), for: .touchUpInside)
+        navigationItem.rightBarButtonItem = UIBarButtonItem(customView: seaarhcButton)
+
+        navigationController?.navigationBar.tintColor = .white
+        navigationController?.navigationBar.standardAppearance.configureWithTransparentBackground()
 
         let tapGesture = UITapGestureRecognizer(target: self, action: #selector(mapDidTap))
         mapView.addGestureRecognizer(tapGesture)
     }
 
     override func viewDidAppear(_ animated: Bool) {
-        determineCurrentLocation()
+        if let location = locationManager.location {
+            mapView.centerToLocation(location)
+        }
     }
 
-    func determineCurrentLocation() {
-        locationManager = CLLocationManager()
-        locationManager.desiredAccuracy = kCLLocationAccuracyBest
-        locationManager.requestAlwaysAuthorization()
+    override func viewWillDisappear(_ animated: Bool) {
+        weatherViewModel.fetchWeather(in: currentLocation)
+    }
 
-        if CLLocationManager.locationServicesEnabled() {
-            locationManager.startUpdatingLocation()
-            if let location = locationManager.location {
-                mapView.centerToLocation(location)
-            }
-        }
+    @objc private func searchButtonDidTap() {
     }
 
     @objc private func mapDidTap(gestureRecognizer: UIGestureRecognizer) {
@@ -58,21 +68,27 @@ final class MapViewController: UIViewController, UIGestureRecognizerDelegate {
         let locationCoordinale = mapView.convert(locationTouch, toCoordinateFrom: mapView)
         let pin = MKPointAnnotation()
         pin.coordinate = locationCoordinale
-        pin.title = "What weather is here?\n" + setUsersClosestLocation(lattitude: locationCoordinale.latitude, longitude: locationCoordinale.longitude)
+        pin.title = "What weather is here?"
+        let location = CLLocation(
+            latitude: locationCoordinale.latitude,
+            longitude: locationCoordinale.longitude
+        )
+        currentLocation = location
         mapView.addAnnotation(pin)
+//        mapView.centerToLocation(location)
     }
 
     func setUsersClosestLocation(lattitude: CLLocationDegrees, longitude: CLLocationDegrees) -> String {
         let geoCoder = CLGeocoder()
         let location = CLLocation(latitude: lattitude, longitude: longitude)
-
+        var currentLocationStr:String = ""
         geoCoder.reverseGeocodeLocation(location) {
             placemarks, _ in
             if let mPlacemark = placemarks {
                 if let dict = mPlacemark[0].addressDictionary as? [String: Any] {
                     if let Name = dict["Name"] as? String {
                         if let City = dict["City"] as? String {
-                            self.currentLocationStr = Name + ", " + City
+                            currentLocationStr = Name + ", " + City
                         }
                     }
                 }
@@ -81,6 +97,8 @@ final class MapViewController: UIViewController, UIGestureRecognizerDelegate {
         return currentLocationStr
     }
 }
+
+//MARK: Map View 
 
 private extension MKMapView {
     func centerToLocation(
@@ -92,5 +110,13 @@ private extension MKMapView {
             latitudinalMeters: regionRadius,
             longitudinalMeters: regionRadius)
         setRegion(coordinateRegion, animated: true)
+    }
+}
+
+// MARK: Search Results Updating
+
+extension MapViewController: UISearchResultsUpdating {
+    func updateSearchResults(for searchController: UISearchController) {
+        print("search")
     }
 }
